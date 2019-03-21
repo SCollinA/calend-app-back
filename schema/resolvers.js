759e43c003dcae2d07f80ad88a5938edff4d90ab
Collin Argo <collinargo@gmail.com>
2019-03-21 19:06:21 -0400

    use args.input

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -26,16 +26,16 @@ const resolvers = {
     }),
     Query: {
         getUser: (obj, args, context, info) => {
-            return User.findOne(args)
+            return User.findOne(args.input)
         },
         getEvent: (obj, args, context, info) => {
-            return Event.findOne(args)
+            return Event.findOne(args.input)
         },
         getUsers: (obj, args, context, info) => {
-            return User.find(args)
+            return User.find(args.input)
         },
         getEvents: (obj, args, context, info) => {
-            return Event.find(args)
+            return Event.find(args.input)
         },
         getAllUsers: (obj, args, context, info) => {
             return User.find()
@@ -46,22 +46,23 @@ const resolvers = {
     },
     Mutation: {
         addUser: (obj, args, context, info) => {
-            return User.create(args)
+            console.log(args.input)
+            return User.create(args.input)
         },
         updateUser: (obj, args, context, info) => {
-            return User.findByIdAndUpdate(args.id, args)
+            return User.findByIdAndUpdate(args.id, args.input)
         },
         removeUser: (obj, args, context, info) => {
-            return User.remove(args)
+            return User.remove(args.input)
         },
         addEvent: (obj, args, context, info) => {
-            return Event.create(args)
+            return Event.create(args.input)
         },
         updateEvent: (obj, args, context, info) => {
-            return Event.findByIdAndUpdate(args.id, args)
+            return Event.findByIdAndUpdate(args.id, args.input)
         },
         removeEvent: (obj, args, context, info) => {
-            return Event.remove(args)
+            return Event.remove(args.input)
         },
     },
 }
