0bef07c9bd60a14a0e083eda3ede82f6db02bac9
Collin Argo <collinargo@gmail.com>
2019-03-21 16:58:46 -0400

    setup apollo server

--- index.js
+++ index.js
@@ -6,3 +6,42 @@ import http from 'http'
 import https from 'https'
 import bodyparser from 'body-parser'
 require('dotenv').config()
+
+const configurations = {
+    production: { ssl: false, port: 4000, hostname: 'localhost' },
+    development: { ssl: false, port: 4000, hostname: 'localhost' }
+}
+
+const environment = process.env.NODE_ENV || 'production'
+const config = configurations[environment]
+
+const apollo = new ApolloServer({
+    typeDefs,
+    resolvers,
+    context: ({ req, connection }) => {
+        if (connection) { // this is a subscription
+            return connection.context
+        } else {
+            const authorization = req.headers.authorization || ''
+            return { authorization }
+    }},
+    cors: {
+        origin: [
+            'http://localhost:3000', // dev client port
+            'https://calnd.app' // prod url
+]}})
+
+const app = express()
+
+apollo.applyMiddleware({ app })
+
+const server = http.createServer(app)
+
+apollo.installSubscriptionHandlers(server)
+
+server.listen({
+        ...config
+    }, () => console.log(
+        '🚀 Server ready at',
+        `http${config.ssl ? 's' : ''}://${config.hostname}:${config.port}${apollo.graphqlPath}`
+))
